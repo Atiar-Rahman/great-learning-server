08af76e5f5196358c06d522f4f8957e79cf9a75b
Atiar-Rahman <atiar1709@gmail.com>
2025-03-10 08:59:06 +0600

    basic servers setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware
+app.use(cors())
+app.use(express.json())
+
+
+//routers
+
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(port,()=>{
+    console.log(`great learning server running port on: ${port}`);
+})
